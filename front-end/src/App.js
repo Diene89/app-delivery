d37abed7175d288a4df9829ead480d4c0f3e4e5c
matheus jesse <matheus_jesse@hotmail.com>
2022-09-20 19:04:24 -0300

    feat: pagina Login form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import GlobalStyle from './assets/styles/global';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <>
+      <Login />
+      <GlobalStyle />
+    </>
   );
 }
 
