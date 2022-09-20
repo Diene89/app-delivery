49c1d55398dafc74f57770e8a44fcf4e98e9dbc8
Denis Jonathan <denisjonathan7@gmail.com>
2022-09-20 16:56:04 -0300

    feat: create sales products migrate

---
+++ back-end/src/database/migrations/20220920192600-create-sales-products.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('SalesProducts', {
+      saleId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        field: 'sale_id'
+      },
+      productId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        field: 'product_id'
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('salesProducts');
+  }
+};
