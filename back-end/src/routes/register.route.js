fabf6c2f9a0e80122b69e47bd3f0399b84d0d967
Tiemi Faustino <hayashipharma@gmail.com>
2022-09-21 16:26:54 -0300

    feat: Cria route para /register

---
+++ back-end/src/routes/register.route.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const registerController = require('../controllers/register.controller')
+
+const registerRoute = Router();
+
+registerRoute.post('/', registerController.create);
+
+module.exports = registerRoute;
