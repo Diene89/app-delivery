6ac7bd9f6ae6146c53c3c25cfb70b419c293d567
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-21 18:49:40 -0300

    build: Adicionando CORS no App para liberar acesso ao FrontEnd

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 
 const loginRoute = require('../routes/login.route');
 const registerRoute = require('../routes/register.route');
@@ -7,6 +8,7 @@ const productsRoute = require('../routes/products.route');
 const middlewareError = require('../middlewares/errors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(express.json());
