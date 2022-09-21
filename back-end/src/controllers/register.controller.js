f997c4141da1d16806d70540a0eb1144220a2a8e
Tiemi Faustino <hayashipharma@gmail.com>
2022-09-21 18:36:20 -0300

    feat: Adiciona retorno de token ao cadastrar um usuário

--- back-end/src/controllers/register.controller.js
+++ back-end/src/controllers/register.controller.js
@@ -1,10 +1,12 @@
 const registerService = require('../services/register.service');
+const jwtService = require('../services/utils/jwtService');
 
 const registerController = {
   async create(req, res) {
     const data = registerService.validateRegisterBody(req.body);
     await registerService.create(data);
-    return res.status(201).end();
+    const token = jwtService.createToken(req.body.email);
+    return res.status(201).json({ token });
   },
 };
 
