68c35430c6529935a272ed5d9ebbdece466181c7
Tiemi Faustino <hayashipharma@gmail.com>
2022-09-21 16:27:12 -0300

    feat: cria service para /register

---
+++ back-end/src/services/register.service.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+const db = require('../database/models');
+const md5 = require('md5');
+
+const registerService = {
+  validateRegisterBody(data) {
+    const bodySchema = Joi.object({
+      name: Joi.string().max(12).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    });
+  
+    const { error, value } = bodySchema.validate(data);
+    if (error) {
+      error.name = 'ValidationError';
+      throw error;
+    }
+    return value;
+  },
+
+  async create({ name, email, password}) {
+    const checkIfUser = await db.user.findOne({
+      where: { email },
+    });
+
+    if (checkIfUser) {
+      const error = new Error('User already registered');
+      error.name = 'ConflictError';
+      throw error;
+    }
+
+    const cryptoPassword = md5(password);
+
+    await db.user.create({
+      name,
+      email,
+      password: cryptoPassword,
+      role: 'customer'
+    })
+  }
+}
+
+module.exports = registerService;
