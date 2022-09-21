2f9c0d8f16122b92c165655ee9654f8ca7dbef2f
Diene89 <diene_89@hotmail.com>
2022-09-21 16:04:32 -0300

    feat: create products controller

--- back-end/src/controllers/products.controller.js
+++ back-end/src/controllers/products.controller.js
@@ -1,1 +1,9 @@
+const Product = require('../services/products.service')
 
+module.exports = {
+    async products() {
+        const result = await Product.listProducts();
+
+        return res.status(200).json(result);
+    }
+}
