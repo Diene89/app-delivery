2d64892c806a94e455eb694cf35d54b6374ac938
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-20 23:17:27 -0300

    feat: Funções JWT para criar e validar token

---
+++ back-end/src/services/utils/jwtService.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+import jwt from 'jsonwebtoken';
+
+const jwtService = {
+  createToken(data) {
+    const token = jwt.sign(data, process.env.JWT_SECRET);
+
+    return token;
+  },
+
+  validateToken(token) {
+    if (!token) {
+      const error = new Error('Token not Found');
+      error.name = 'UnauthorizedError';
+      throw error;
+    }
+
+    try {
+      const data = jwt.verify(token, process.env.JWT_SECRET);
+
+      return data;
+    } catch (e) {
+      const error = new Error('Expired or invalid token');
+      error.name = 'UnauthorizedError';
+      throw error;
+    }
+  },
+};
+
+export default jwtService;
