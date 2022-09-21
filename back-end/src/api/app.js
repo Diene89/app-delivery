8e76111875667bfc0b693ff9e1ae6c97cce4ac95
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-21 15:24:44 -0300

    build: Importando express-async-errors no app e habilitando para reconhecer JSON

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('express-async-errors');
 const loginRoute = require('../routes/login.route');
 const middlewareError = require('../middlewares/errors');
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/login', loginRoute);
 
