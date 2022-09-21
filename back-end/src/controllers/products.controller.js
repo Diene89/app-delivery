a3c3bf42100aabcca8aff3c846ca9a78249e2db3
Diene89 <diene_89@hotmail.com>
2022-09-21 17:50:49 -0300

    fix: Corrige controller/products

--- back-end/src/controllers/products.controller.js
+++ back-end/src/controllers/products.controller.js
@@ -3,7 +3,6 @@ const Product = require('../services/products.service')
 module.exports = {
     async listProducts(_req, res) {
         const result = await Product.listProducts();
-      
         return res.status(200).json(result);
     },
 };
