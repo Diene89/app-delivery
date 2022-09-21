a11765614de522bde290769cf561026af4df7a9d
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-21 01:03:48 -0300

    feat: Middleware de erro

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const loginRoute = require('../routes/login.route');
+const middlewareError = require('../middlewares/errors');
 
 const app = express();
 
 app.use('/login', loginRoute);
+
+app.use(middlewareError);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
