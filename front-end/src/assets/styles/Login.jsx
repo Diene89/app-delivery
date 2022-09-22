0ece586c34c02f6e65dd3ab0a84ddc922b127eab
matheus jesse <matheus_jesse@hotmail.com>
2022-09-22 11:14:38 -0300

    style: estilo do form e do botão da page de login

--- front-end/src/assets/styles/Login.jsx
+++ front-end/src/assets/styles/Login.jsx
@@ -6,16 +6,72 @@ const ContainerLogin = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  
+  img {
+    width: 120px;
+    height: 120px;
+    margin: 30px;
+  }
 
   .formLogin {
     display: flex;
     justify-content: center;
     align-items: center;
+    align-content: flex-start;
     flex-wrap: wrap;
     border: 1px solid black;
     width: 400px;
-    height:500px;
+    height:540px;
+    border-radius: 24px;
   }
+
+  .formLogin form {
+    height: 290px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 10px;
+  }
+  
+  .formLogin form label{
+    display: flex;
+    width: 300px;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin-bottom: 20px;
+    font-size: 1em;
+  }
+
+  .formLogin form label input{
+    width:290px;
+    height: 48px;
+    border-radius: 24px;
+    padding-left: 20px;
+    font-size: 1.2em;
+  }
+
+  button {
+    width: 290px;
+    height: 48px;
+    margin-bottom: 12px;
+    border-radius: 10px;
+    font-size: 1.2em;
+  }
+
+  .register-button {
+    background: white;
+    border: none;
+    color: black;
+  }
+
+  .login-button {
+    background: white;    
+  }
+
+  .login-button: disabled {
+    background: white;    
+  }
+
 `;
 
 export default ContainerLogin;
