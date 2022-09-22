499689f97286665aed9d58163e948549f6fa0c87
Tiemi Faustino <hayashipharma@gmail.com>
2022-09-22 17:28:36 -0300

    feat: cria service para rota /seller/orders

--- back-end/src/services/seller.service.js
+++ back-end/src/services/seller.service.js
@@ -1,14 +1,35 @@
 const db = require('../database/models');
+const jwtService = require('./utils/jwtService');
 
 module.exports = {
-  async getSalesByIdSeller(sellerId) {
+  async getSalesBySeller(token) {
+    const { id } = jwtService.decodeToken(token);
+
     const salesBySeller = await db.sales.findAll({
-      where: { sellerId },
+      where: { sellerId: id },
       include: [
         { model: db.products, as: 'products' },
       ],
     });
 
+    if (!salesBySeller.length) {
+      return { message: 'Você não possui nenhuma venda' };
+    }
+
     return salesBySeller;
+  },
+
+  async getSaleByIdOrder(id) {
+    const sale = await db.sales.findByPk(id, {
+      include: [
+        { model: db.products, as: 'products' },
+      ],
+    });
+
+    if (!sale) {
+      return { message: 'Venda não encontrada' };
+    }
+
+    return sale;
   },
 };
