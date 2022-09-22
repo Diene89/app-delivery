c498256580b7d7a577446881f843c0f4f0a556f0
matheus jesse <matheus_jesse@hotmail.com>
2022-09-22 16:55:04 -0300

    feat: page products html tags

---
+++ front-end/src/pages/Products.jsx
@@ -0,0 +1,15 @@
+import ContainerProducts from '../assets/styles/Login';
+import Navbar from '../components/Navbar';
+
+function Products() {
+  return (
+    <>
+      <Navbar />
+      <ContainerProducts>
+        <p> a </p>
+      </ContainerProducts>
+    </>
+  );
+}
+
+export default Products;
