899555247f85b2ef6935854adc9d39c4c09c9bc5
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-22 00:46:25 -0300

    feat: Rota para buscar sale pelo id

--- back-end/src/routes/sales.route.js
+++ back-end/src/routes/sales.route.js
@@ -4,5 +4,6 @@ const salesController = require('../controllers/sales.controller');
 const salesRoute = Router();
 
 salesRoute.get('/', salesController.getAllSales);
+salesRoute.get('/:id', salesController.getSaleById);
 
 module.exports = salesRoute;
