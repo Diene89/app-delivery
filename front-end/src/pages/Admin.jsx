00b2bad2404a294f8d69c2d449312ea543a360f9
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-23 15:38:57 -0300

    feat: Rota e estilo inicial da página Admin

---
+++ front-end/src/pages/Admin.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+import ContainerAdmin from '../assets/styles/Admin';
+
+function Admin() {
+  /* const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userRole, setUserRole] = useState(''); */
+
+  return (
+    <ContainerAdmin>
+      <h1>Cadastrar novo usuário</h1>
+    </ContainerAdmin>
+  );
+}
+
+export default Admin;
