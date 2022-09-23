1b50dbafc4049f5decd823ea66b8bb9f23a34b6f
Denis Jonathan <denisjonathan7@gmail.com>
2022-09-23 17:13:26 -0300

    feat: create Details and Address component to Checkout page

---
+++ front-end/src/components/CheckoutDetailsAndAddress/index.jsx
@@ -0,0 +1,66 @@
+import ContainerDetailsAndAddress from './style';
+import selectArrow from '../../assets/images/select-arrow.png';
+
+function CheckoutDetailsAndAddress() {
+  const mockedSellers = ['Denis', 'Didi', 'Tiemi', 'Alexandre', 'Matheus'];
+
+  return (
+    <ContainerDetailsAndAddress>
+      <h2 className="checkout-details-title">Detalhes e Endereço para Entrega</h2>
+
+      <div className="checkout-details-container">
+        <div className="checkout-details-options-container">
+          <div className="checkout-details-option">
+            <span className="checkout-details-subtitle">P. Vendedora Responsável:</span>
+
+            <select
+              className="checkout-detailts-input"
+              data-testid="customer_checkout__select-seller"
+            >
+              <option value="selecione" selected disabled>Selecione</option>
+
+              {mockedSellers.map((name, index) => {
+                const keyId = `${name}__${index}`;
+
+                return <option key={ keyId } value={ name }>{name}</option>;
+              })}
+            </select>
+          </div>
+
+          <div className="checkout-details-option">
+            <span className="checkout-details-subtitle">Endereço</span>
+
+            <input
+              className="checkout-detailts-input"
+              data-testid="customer_checkout__input-address"
+              type="text"
+              placeholder="Digite seu endereço"
+            />
+          </div>
+
+          <div className="checkout-details-option">
+            <span className="checkout-details-subtitle">Número</span>
+
+            <input
+              className="checkout-detailts-input"
+              data-testid="customer_checkout__input-addressNumber"
+              type="text"
+              placeholder="Número do seu endereço"
+            />
+          </div>
+        </div>
+
+        <div className="checkout-details-finish-button-container">
+          <button
+            data-testid="customer_checkout__button-submit-order"
+            type="button"
+          >
+            <span>FINALIZAR PEDIDO</span>
+          </button>
+        </div>
+      </div>
+    </ContainerDetailsAndAddress>
+  );
+}
+
+export default CheckoutDetailsAndAddress;
