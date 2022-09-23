5d900e145dfb110b4e483a0bf9d08976782f7e99
Tiemi Faustino <hayashipharma@gmail.com>
2022-09-23 20:00:16 -0300

    feat: Cria função getUsers e deleteUsers para interagir com a tabela de usuários da página de admin

--- front-end/src/api/requestAdmin.js
+++ front-end/src/api/requestAdmin.js
@@ -1,6 +1,8 @@
+const endpoint = 'http://localhost:3001/admin/manage';
+
 async function requestAdmin(userData) {
   try {
-    const response = await fetch('http://localhost:3001/admin/manage', {
+    const response = await fetch(endpoint, {
       method: 'POST',
       body: JSON.stringify(userData),
       headers: {
@@ -13,6 +15,26 @@ async function requestAdmin(userData) {
     return data;
   } catch (error) {
     console.log('Algo deu errado ao tentar realizar o cadastro do usuário', error);
+  }
+}
+
+export async function getUsers() {
+  try {
+    const response = await fetch(endpoint);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteUsers(id) {
+  try {
+    await fetch(`${endpoint}/${id}`, {
+      method: 'DELETE',
+    });
+  } catch (error) {
+    console.log('Erro ao deletar usuário', error);
   }
 }
 
