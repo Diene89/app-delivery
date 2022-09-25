bbffd4b51d22e34e95585a8f2e4a5e7d5fd9438f
matheus jesse <matheus_jesse@hotmail.com>
2022-09-25 20:59:17 -0300

    style: cardProducts

--- front-end/src/assets/styles/Products.jsx
+++ front-end/src/assets/styles/Products.jsx
@@ -6,7 +6,8 @@ const ContainerProducts = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: 290px;
+
+    position:absolute; top:240px;
 
     .products-section {
       max-width: 1446px;
@@ -14,6 +15,16 @@ const ContainerProducts = styled.div`
       height: 900px;
       display flex;
       flex-wrap: wrap;
+      justify-content: center;
+      align-content: flex-start;
+    }
+
+    .total-button {
+      position: fixed;
+      bottom: 70px;
+      right: 40px;
+      height: 80px;
+      width: 20%;
     }
  
 `;
