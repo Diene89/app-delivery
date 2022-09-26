c2446f473852124fce72d4b52723f75428beb3f6
Denis Jonathan <denisjonathan7@gmail.com>
2022-09-26 16:54:20 -0300

    feat: use real data in checkout orders section and create function to remove item of list

--- front-end/src/components/CheckoutOrders/index.jsx
+++ front-end/src/components/CheckoutOrders/index.jsx
@@ -1,83 +1,36 @@
+import { useEffect, useState } from 'react';
 import CheckoutTable from '../CheckoutTable';
 import ContainerOrders from './style';
 
-const mockedItems = [
-  {
-    id: 1,
-    description: 'Cerveja Stella 250ml',
-    quantity: 3,
-    price: 3.50,
-  },
-  {
-    id: 2,
-    description: 'Cerveja Skol Latão 450ml',
-    quantity: 4,
-    price: 4.10,
-  },
-  {
-    id: 3,
-    description: 'Salgadinho Torcida Churrasco',
-    quantity: 1,
-    price: 1.56,
-  },
-  {
-    id: 1,
-    description: 'Cerveja Stella 250ml3 Cerveja Stella 250ml3 Cerveja Stella 250ml3 Cerv',
-    quantity: 3,
-    price: 3.50,
-  },
-  {
-    id: 2,
-    description: 'Cerveja Skol Latão 450ml3',
-    quantity: 4,
-    price: 4.10,
-  },
-  {
-    id: 3,
-    description: 'Salgadinho Torcida Churrasco3',
-    quantity: 1,
-    price: 1.56,
-  },
-  {
-    id: 1,
-    description: 'Cerveja Stella 250ml2',
-    quantity: 3,
-    price: 3.50,
-  },
-  {
-    id: 2,
-    description: 'Cerveja Skol Latão 450ml2',
-    quantity: 4,
-    price: 4.10,
-  },
-  {
-    id: 3,
-    description: 'Salgadinho Torcida Churrasco2',
-    quantity: 1,
-    price: 1.56,
-  },
-  {
-    id: 1,
-    description: 'Cerveja Stella 250ml1',
-    quantity: 3,
-    price: 3.50,
-  },
-  {
-    id: 2,
-    description: 'Cerveja Skol Latão 450ml1',
-    quantity: 4,
-    price: 4.10,
-  },
-  {
-    id: 3,
-    description: 'Salgadinho Torcida Churrasco1',
-    quantity: 1,
-    price: 1.56,
-  },
-];
-
 function CheckoutOrders() {
-  const mockedPrice = 35.78;
+  const [userShopCart, setUserShopCart] = useState([]);
+  const [cartAmount, setCartAmount] = useState(0);
+
+  useEffect(() => {
+    const storagedCart = JSON.parse(localStorage.getItem('user'));
+
+    if (storagedCart.shopCart) {
+      const { shopCart } = storagedCart;
+      setUserShopCart([...shopCart]);
+    }
+  }, []);
+
+  useEffect(() => {
+    const sum = userShopCart.reduce((acc, current) => {
+      const { price, quantity } = current;
+      const tempSum = quantity * price;
+
+      return acc + tempSum;
+    }, 0);
+
+    setCartAmount(sum);
+  }, [userShopCart]);
+
+  const removeItem = (idToRemove) => {
+    const newCart = userShopCart.filter(({ id }) => Number(id) !== Number(idToRemove));
+
+    setUserShopCart(newCart);
+  };
 
   return (
     <ContainerOrders>
@@ -85,7 +38,7 @@ function CheckoutOrders() {
 
       <div className="checkout-orders-container">
         <div className="checkout-orders-table-container">
-          <CheckoutTable items={ mockedItems } />
+          <CheckoutTable items={ userShopCart } removeItem={ removeItem } />
         </div>
 
         <div
@@ -94,7 +47,7 @@ function CheckoutOrders() {
         >
           <span>
             {`Total: ${
-              mockedPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+              cartAmount.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
             }`}
           </span>
         </div>
