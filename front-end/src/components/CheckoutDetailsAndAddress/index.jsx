9e1239da256c7fdadf846e9418a83490d010d517
Denis Jonathan <denisjonathan7@gmail.com>
2022-09-26 16:55:26 -0300

    feat: implement logic to complete checkout

--- front-end/src/components/CheckoutDetailsAndAddress/index.jsx
+++ front-end/src/components/CheckoutDetailsAndAddress/index.jsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from 'react';
+import requestCompleteCheckout from '../../api/requestCompleteCheckout';
+import requestSellers from '../../api/requestSellers';
 import CheckoutSelect from '../CheckoutDetailsSelect';
 import ContainerDetailsAndAddress from './style';
 
 function CheckoutDetailsAndAddress() {
-  const mockedSellers = ['Denis', 'Didi', 'Tiemi', 'Alexandre', 'Matheus'];
+  const mockedSellers = [
+    { name: 'Denis', id: 1 },
+    { name: 'Didi', id: 2 },
+    { name: 'Tiemi', id: 3 },
+    { name: 'Alexandre', id: 4 },
+    { name: 'Matheus', id: 5 },
+  ];
+  const mockedTotalPrice = 28.46;
+
+  const [sellersInfos, setSellersInfo] = useState([]);
+  const [sellerSelected, setSellerSelected] = useState({});
+  const [userAddress, setUserAddress] = useState('');
+  const [userAddressNumber, setUserAddressNumber] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const sellers = await requestSellers() || mockedSellers;
+
+      setSellersInfo(sellers);
+    })();
+  }, []);
+
+  const handleSeller = (target) => {
+    const seller = target.value;
+
+    setSellerSelected(seller);
+  };
+
+  const handleAddress = (target) => {
+    const address = target.value;
+
+    setUserAddress(address);
+  };
+
+  const handleAddressNumber = (target) => {
+    const addressNumber = target.value;
+
+    setUserAddressNumber(addressNumber);
+  };
+
+  const handleCompleteCheckout = async () => {
+    const { shopCart, token } = localStorage.getItem('user');
+    const { id: sellerId } = sellerSelected;
+
+    const userData = {
+      sellerId,
+      mockedTotalPrice,
+      userAddress,
+      userAddressNumber,
+      shopCart,
+    };
+
+    const checkCheckout = requestCompleteCheckout(userData, token);
+
+    return checkCheckout;
+  };
 
   return (
     <ContainerDetailsAndAddress>
@@ -13,7 +71,7 @@ function CheckoutDetailsAndAddress() {
           <div className="checkout-details-option">
             <span className="checkout-details-subtitle">P. Vendedora Responsável:</span>
 
-            <CheckoutSelect sellers={ mockedSellers } />
+            <CheckoutSelect sellers={ sellersInfos } handleSeller={ handleSeller } />
           </div>
 
           <div className="checkout-details-option">
@@ -24,6 +82,7 @@ function CheckoutDetailsAndAddress() {
               data-testid="customer_checkout__input-address"
               type="text"
               placeholder="Digite seu endereço"
+              onChange={ (target) => handleAddress(target) }
             />
           </div>
 
@@ -35,6 +94,7 @@ function CheckoutDetailsAndAddress() {
               data-testid="customer_checkout__input-addressNumber"
               type="text"
               placeholder="Número do seu endereço"
+              onChange={ (target) => handleAddressNumber(target) }
             />
           </div>
         </div>
@@ -42,6 +102,7 @@ function CheckoutDetailsAndAddress() {
         <div className="checkout-details-finish-button-container">
           <button
             data-testid="customer_checkout__button-submit-order"
+            onClick={ handleCompleteCheckout }
             type="button"
           >
             <span>FINALIZAR PEDIDO</span>
