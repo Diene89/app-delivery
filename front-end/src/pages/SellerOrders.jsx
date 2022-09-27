08b82d318fa14d002fae5af6a864fe7c181c9c75
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-27 17:16:59 -0300

    feat: Renderização dos pedidos

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import SellerOrdersContainer from '../assets/styles/SellerOrders';
+import { getSellerOrders } from '../api/requestSeller';
 
 function SellerOrders() {
-  const [orders, setOrders] = useState();
+  const [orders, setOrders] = useState(['test']);
 
   async function getOrders() {
-    const arrayOrders = await test();
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    const arrayOrders = await getSellerOrders(token);
+    console.log(arrayOrders);
     setOrders(arrayOrders);
   }
 
@@ -16,7 +19,33 @@ function SellerOrders() {
   return (
     <SellerOrdersContainer>
       { orders && orders.map((order) => (
-        <span key={ order.id }>{order.status}</span>
+        <div key={ order.id }>
+          <span
+            data-testid={ `seller_orders__element-order-id-${order.id}` }
+          >
+            { order.id }
+          </span>
+          <span
+            data-testid={ `seller_orders__element-delivery-status-${order.id}` }
+          >
+            { order.status }
+          </span>
+          <span
+            data-testid={ `seller_orders__element-order-date-${order.id}` }
+          >
+            { order.saleDate }
+          </span>
+          <span
+            data-testid={ `seller_orders__element-card-price-${order.id}` }
+          >
+            { order.totalPrice }
+          </span>
+          <span
+            data-testid={ `seller_orders__element-card-address-${order.id}` }
+          >
+            { `${order.deliveryAddress}, ${order.deliveryNumber}` }
+          </span>
+        </div>
       )) }
     </SellerOrdersContainer>
   );
