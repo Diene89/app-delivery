99361b1f67400c2c42844c79c00856a59d78efa0
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-27 18:59:33 -0300

    feat: Função que faz requisição para atualizar o pedido

--- front-end/src/api/requestSeller.js
+++ front-end/src/api/requestSeller.js
@@ -20,8 +20,24 @@ export async function getSellerOrderById(id) {
   try {
     const response = await fetch(`${endpoint}/${id}`);
 
-    const data = response.json();
+    const data = await response.json();
 
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateOrder(id, status) {
+  try {
+    const response = await fetch(`${endpoint}/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ status }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const data = response.json();
     return data;
   } catch (error) {
     console.log(error);
