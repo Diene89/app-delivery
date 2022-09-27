9cd39dac0dca9a94c1fd70ff93846ec96c326e4e
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-27 18:00:56 -0300

    feat: Componente que renderiza os produtos do pedido

---
+++ front-end/src/components/OrderTable/index.jsx
@@ -0,0 +1,20 @@
+function OrderTable(order) {
+  console.log(order);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Subtotal</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
+
+export default OrderTable;
