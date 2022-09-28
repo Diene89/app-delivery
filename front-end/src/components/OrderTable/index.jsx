6d99d812133851ec266495ea8c3a8f3047c83816
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-27 23:57:03 -0300

    feat: Renderizando produtos na tela de detalhes do pedido do vendedor

--- front-end/src/components/OrderTable/index.jsx
+++ front-end/src/components/OrderTable/index.jsx
@@ -1,5 +1,8 @@
-function OrderTable(order) {
-  console.log(order);
+import PropTypes from 'prop-types';
+
+function OrderTable({ order }) {
+  const { products } = order;
+
   return (
     <div>
       <table>
@@ -12,9 +15,24 @@ function OrderTable(order) {
             <th>Subtotal</th>
           </tr>
         </thead>
+        <tbody>
+          { products && products.map((product, index) => (
+            <tr key={ product.id }>
+              <th>{index + 1}</th>
+              <th>{product.name}</th>
+              <th>{product.salesProducts.quantity}</th>
+              <th>{product.price}</th>
+              <th>{product.price * product.salesProducts.quantity}</th>
+            </tr>
+          )) }
+        </tbody>
       </table>
     </div>
   );
 }
+
+OrderTable.propTypes = {
+  order: PropTypes.shape(PropTypes.string).isRequired,
+};
 
 export default OrderTable;
