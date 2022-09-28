349e13833dfcdeb8ab3352df887b80941a13279a
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-28 15:33:35 -0300

    feat: Adicionando data-testid no elementos e botão de preço total do pedido

--- front-end/src/components/OrderTable/index.jsx
+++ front-end/src/components/OrderTable/index.jsx
@@ -18,15 +18,46 @@ function OrderTable({ order }) {
         <tbody>
           { products && products.map((product, index) => (
             <tr key={ product.id }>
-              <th>{index + 1}</th>
-              <th>{product.name}</th>
-              <th>{product.salesProducts.quantity}</th>
-              <th>{product.price}</th>
-              <th>{product.price * product.salesProducts.quantity}</th>
+              <th
+                data-testid={ `
+                seller_order_details__element-order-table-item-number-${index}` }
+              >
+                {index + 1}
+              </th>
+              <th
+                data-testid={ `
+                seller_order_details__element-order-table-name-${index}` }
+              >
+                {product.name}
+              </th>
+              <th
+                data-testid={ `
+                seller_order_details__element-order-table-quantity-${index}` }
+              >
+                {product.salesProducts.quantity}
+              </th>
+              <th
+                data-testid={ `
+                seller_order_details__element-order-table-unit-price-${index}` }
+              >
+                {product.price}
+              </th>
+              <th
+                data-testid={ `
+                seller_order_details__element-order-table-sub-total-${index}` }
+              >
+                {(product.price * product.salesProducts.quantity).toFixed(2)}
+              </th>
             </tr>
           )) }
         </tbody>
       </table>
+      <button
+        type="button"
+        data-testid="seller_order_details__element-order-total-price"
+      >
+        {order.totalPrice.toString().replace('.', ',')}
+      </button>
     </div>
   );
 }
