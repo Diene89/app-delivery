5b9436caf66050fa823f4b3cce90f83e7ebe5fd5
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-09-27 23:44:09 -0300

    feat: Redirecionando para a página de detalhes do pedido se clicar no seu card

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SellerOrdersContainer from '../assets/styles/SellerOrders';
 import { getSellerOrders } from '../api/requestSeller';
 
 function SellerOrders() {
   const [orders, setOrders] = useState(['test']);
+  const navigate = useNavigate();
 
   async function getOrders() {
     const { token } = JSON.parse(localStorage.getItem('user'));
     const arrayOrders = await getSellerOrders(token);
     setOrders(arrayOrders);
+  }
+
+  function rediretToDetails(id) {
+    navigate(`/seller/orders/${id}`);
   }
 
   useEffect(() => {
@@ -19,31 +25,33 @@ function SellerOrders() {
     <SellerOrdersContainer>
       { orders && orders.map((order) => (
         <div key={ order.id }>
-          <span
-            data-testid={ `seller_orders__element-order-id-${order.id}` }
-          >
-            { order.id }
-          </span>
-          <span
-            data-testid={ `seller_orders__element-delivery-status-${order.id}` }
-          >
-            { order.status }
-          </span>
-          <span
-            data-testid={ `seller_orders__element-order-date-${order.id}` }
-          >
-            { order.saleDate }
-          </span>
-          <span
-            data-testid={ `seller_orders__element-card-price-${order.id}` }
-          >
-            { order.totalPrice }
-          </span>
-          <span
-            data-testid={ `seller_orders__element-card-address-${order.id}` }
-          >
-            { `${order.deliveryAddress}, ${order.deliveryNumber}` }
-          </span>
+          <button type="button" onClick={ () => rediretToDetails(order.id) }>
+            <span
+              data-testid={ `seller_orders__element-order-id-${order.id}` }
+            >
+              { order.id }
+            </span>
+            <span
+              data-testid={ `seller_orders__element-delivery-status-${order.id}` }
+            >
+              { order.status }
+            </span>
+            <span
+              data-testid={ `seller_orders__element-order-date-${order.id}` }
+            >
+              { order.saleDate }
+            </span>
+            <span
+              data-testid={ `seller_orders__element-card-price-${order.id}` }
+            >
+              { order.totalPrice }
+            </span>
+            <span
+              data-testid={ `seller_orders__element-card-address-${order.id}` }
+            >
+              { `${order.deliveryAddress}, ${order.deliveryNumber}` }
+            </span>
+          </button>
         </div>
       )) }
     </SellerOrdersContainer>
