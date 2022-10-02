1eeed01cdb8786ada7ca2b675320269a6aa59a80
Denis Jonathan <denisjonathan7@gmail.com>
2022-10-02 09:48:50 -0300

    style: refactor products page

--- front-end/src/assets/styles/Products.jsx
+++ front-end/src/assets/styles/Products.jsx
@@ -5,24 +5,49 @@ const ContainerProducts = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+  position: relative;
   width: 100vw;
 
   .products-section {
-    max-width: 1446px;
-    min-width: 400px;
-    height: 900px;
+    align-content: flex-start;
     display: flex;
     flex-wrap: wrap;
+    gap: 20px;
     justify-content: center;
-    align-content: flex-start;
+    margin-top: 20px;
+    max-height: 435px;
+    overflow-y: scroll;
+    padding: 5px 0;
+    width: 85%;
+  }
+
+  .products-section::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  .products-section::-webkit-scrollbar-track {
+    background: #fafafa;
+  }
+
+  .products-section::-webkit-scrollbar-thumb {
+    background-color: #73aadd;
+    border: 3px none #ffffff;
+    border-radius: 10px;
   }
 
   .total-button {
-    position: fixed;
-    bottom: 10%;
-    right: 5%;
-    height: 80px;
-    width: 20%;
+    align-self: flex-end;
+    background: linear-gradient(180deg, #5C97CD 0%, #73AADD 50.52%, #48B3EF 100%);
+    border: none;
+    border-radius: 4px;
+    box-shadow: 1px 1px 8px 1px rgba(0, 0, 0, 0.25);
+    color: #fff;
+    font-size: 24px;
+    height: fit-content;
+    margin-right: 110px;
+    margin-top: 17px;
+    padding: 10px 15px;
+    width: fit-content;
   }
 `;
 
