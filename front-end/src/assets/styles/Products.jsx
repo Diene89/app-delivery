9353ca35dc9631dba8695105061e2775d8105adc
Denis Jonathan <denisjonathan7@gmail.com>
2022-10-03 17:33:00 -0300

    feat: create and stylizes OrdersPage and fix many bugs

--- front-end/src/assets/styles/Products.jsx
+++ front-end/src/assets/styles/Products.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const ContainerProducts = styled.div`
   align-items: center;
+  background-color: #F5F6F6;
   display: flex;
   flex-direction: column;
   height: 100vh;
