5fc54aaea71cdc36bf3016524fa14465def294dc
Denis Jonathan <denisjonathan7@gmail.com>
2022-10-04 18:53:10 -0300

    feat: create input to apply voucher

--- front-end/src/components/CheckoutOrders/style.jsx
+++ front-end/src/components/CheckoutOrders/style.jsx
@@ -49,11 +49,66 @@ const CheckoutOrdersContainer = styled.div`
     border-radius: 10px;
   }
 
-  .checkout-orders-amount-container {
+  .checkout-orders-voucher {
     display: flex;
-    justify-content: flex-end;
-    padding: 5px 15px 0;
-    padding-right: 15px;
+    gap: 10px;
+  }
+
+  .checkout-orders-voucher-input {
+    background: #FFFFFF;
+    border: 1px solid #001813;
+    border-radius: 5px;
+    height: 36px;
+    padding-left: 19px;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 28px;
+  }
+
+  .checkout-orders-voucher-input::placeholder {
+    color: #828282;
+  }
+
+  .checkout-orders-voucher-btn {
+    align-items: center;
+    background: linear-gradient(180deg, #5C97CD 0%, #73AADD 50.52%, #48B3EF 100%);
+    border: none;
+    border-radius: 4px;
+    box-shadow: 1px 1px 2px 1px rgb(0 0 0 / 25%);
+    color: #fff;
+    cursor: pointer;
+    display: flex;
+    font-family: Roboto, sans-serif;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    height: fit-content;
+    line-height: 26px;
+    padding: 5px 35px;
+    text-align: center;
+    width: fit-content;
+  }
+
+  .checkout-orders-voucher-btn:hover {
+    background: linear-gradient(180deg, #5C97CD 0%, #438fd6 50.52%, #1a9eeb 100%);
+  }
+
+  .checkout-orders-invalid-voucher {
+    align-self: center;
+    color: red;
+    font-family: Roboto;
+    font-size: 24px;
+    font-weight: 500;
+  }
+
+  .checkout-orders-voucher-and-amount-container {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 17px 0 0;
   }
 
   .checkout-orders-amount {
